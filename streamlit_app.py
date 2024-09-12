ad0ab0ff0b18b059275bec10700f77951403c2da
kaoToto <97693781+kaoToto@users.noreply.github.com>
2024-09-12 17:54:17 +0000

    first personalized version

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title="Inventory tracker",
+    page_title="NWO transfers",
     page_icon=":shopping_bags:",  # This is an emoji shortcode. Could be a URL too.
 )
 
@@ -21,7 +21,7 @@ st.set_page_config(
 def connect_db():
     """Connects to the sqlite database."""
 
-    DB_FILENAME = Path(__file__).parent / "inventory.db"
+    DB_FILENAME = Path(__file__).parent / "players.db"
     db_already_exists = DB_FILENAME.exists()
 
     conn = sqlite3.connect(DB_FILENAME)
@@ -31,71 +31,46 @@ def connect_db():
 
 
 def initialize_data(conn):
-    """Initializes the inventory table with some data."""
+    """Initializes the players table with some data."""
     cursor = conn.cursor()
 
     cursor.execute(
         """
-        CREATE TABLE IF NOT EXISTS inventory (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            item_name TEXT,
-            price REAL,
-            units_sold INTEGER,
-            units_left INTEGER,
-            cost_price REAL,
-            reorder_point INTEGER,
-            description TEXT
+      
+       
+        CREATE TABLE IF NOT EXISTS players (
+            player_id INTEGER PRIMARY KEY,
+            player_name TEXT,
+            clan ENUM ( 'BRA', 'RES','SH', 'GUEST_STAR', 'GUEST')
         )
         """
     )
 
     cursor.execute(
         """
-        INSERT INTO inventory
-            (item_name, price, units_sold, units_left, cost_price, reorder_point, description)
+        INSERT INTO players
+            (player_name, player_id , clan)
         VALUES
-            -- Beverages
-            ('Bottled Water (500ml)', 1.50, 115, 15, 0.80, 16, 'Hydrating bottled water'),
-            ('Soda (355ml)', 2.00, 93, 8, 1.20, 10, 'Carbonated soft drink'),
-            ('Energy Drink (250ml)', 2.50, 12, 18, 1.50, 8, 'High-caffeine energy drink'),
-            ('Coffee (hot, large)', 2.75, 11, 14, 1.80, 5, 'Freshly brewed hot coffee'),
-            ('Juice (200ml)', 2.25, 11, 9, 1.30, 5, 'Fruit juice blend'),
+            -- RES
+            ('Toto', 3053783 , 'RES'),
+           
+            -- SH
+            ('Sandokan', 13567860 , 'SH'),
 
-            -- Snacks
-            ('Potato Chips (small)', 2.00, 34, 16, 1.00, 10, 'Salted and crispy potato chips'),
-            ('Candy Bar', 1.50, 6, 19, 0.80, 15, 'Chocolate and candy bar'),
-            ('Granola Bar', 2.25, 3, 12, 1.30, 8, 'Healthy and nutritious granola bar'),
-            ('Cookies (pack of 6)', 2.50, 8, 8, 1.50, 5, 'Soft and chewy cookies'),
-            ('Fruit Snack Pack', 1.75, 5, 10, 1.00, 8, 'Assortment of dried fruits and nuts'),
+            -- BRA
+            ('Ceara', 7458701 , 'SH')
 
-            -- Personal Care
-            ('Toothpaste', 3.50, 1, 9, 2.00, 5, 'Minty toothpaste for oral hygiene'),
-            ('Hand Sanitizer (small)', 2.00, 2, 13, 1.20, 8, 'Small sanitizer bottle for on-the-go'),
-            ('Pain Relievers (pack)', 5.00, 1, 5, 3.00, 3, 'Over-the-counter pain relief medication'),
-            ('Bandages (box)', 3.00, 0, 10, 2.00, 5, 'Box of adhesive bandages for minor cuts'),
-            ('Sunscreen (small)', 5.50, 6, 5, 3.50, 3, 'Small bottle of sunscreen for sun protection'),
-
-            -- Household
-            ('Batteries (AA, pack of 4)', 4.00, 1, 5, 2.50, 3, 'Pack of 4 AA batteries'),
-            ('Light Bulbs (LED, 2-pack)', 6.00, 3, 3, 4.00, 2, 'Energy-efficient LED light bulbs'),
-            ('Trash Bags (small, 10-pack)', 3.00, 5, 10, 2.00, 5, 'Small trash bags for everyday use'),
-            ('Paper Towels (single roll)', 2.50, 3, 8, 1.50, 5, 'Single roll of paper towels'),
-            ('Multi-Surface Cleaner', 4.50, 2, 5, 3.00, 3, 'All-purpose cleaning spray'),
-
-            -- Others
-            ('Lottery Tickets', 2.00, 17, 20, 1.50, 10, 'Assorted lottery tickets'),
-            ('Newspaper', 1.50, 22, 20, 1.00, 5, 'Daily newspaper')
         """
     )
     conn.commit()
 
 
 def load_data(conn):
-    """Loads the inventory data from the database."""
+    """Loads the players data from the database."""
     cursor = conn.cursor()
 
     try:
-        cursor.execute("SELECT * FROM inventory")
+        cursor.execute("SELECT * FROM players")
         data = cursor.fetchall()
     except:
         return None
@@ -103,14 +78,9 @@ def load_data(conn):
     df = pd.DataFrame(
         data,
         columns=[
-            "id",
-            "item_name",
-            "price",
-            "units_sold",
-            "units_left",
-            "cost_price",
-            "reorder_point",
-            "description",
+            "player_name",
+            "player_id",
+            "clan",
         ],
     )
 
@@ -118,11 +88,11 @@ def load_data(conn):
 
 
 def update_data(conn, df, changes):
-    """Updates the inventory data in the database."""
+    """Updates the players data in the database."""
     cursor = conn.cursor()
 
     if changes["edited_rows"]:
-        deltas = st.session_state.inventory_table["edited_rows"]
+        deltas = st.session_state.player_table["edited_rows"]
         rows = []
 
         for i, delta in deltas.items():
@@ -132,16 +102,11 @@ def update_data(conn, df, changes):
 
         cursor.executemany(
             """
-            UPDATE inventory
+            UPDATE players
             SET
-                item_name = :item_name,
-                price = :price,
-                units_sold = :units_sold,
-                units_left = :units_left,
-                cost_price = :cost_price,
-                reorder_point = :reorder_point,
-                description = :description
-            WHERE id = :id
+                player_name = :player_name,
+                clan = :clan
+            WHERE player_id = :player_id
             """,
             rows,
         )
@@ -149,32 +114,32 @@ def update_data(conn, df, changes):
     if changes["added_rows"]:
         cursor.executemany(
             """
-            INSERT INTO inventory
-                (id, item_name, price, units_sold, units_left, cost_price, reorder_point, description)
+            INSERT INTO players
+               ( player_id, player_name , clan)
             VALUES
-                (:id, :item_name, :price, :units_sold, :units_left, :cost_price, :reorder_point, :description)
+                (:player_id, :player_name, :clan)
             """,
             (defaultdict(lambda: None, row) for row in changes["added_rows"]),
         )
 
     if changes["deleted_rows"]:
         cursor.executemany(
-            "DELETE FROM inventory WHERE id = :id",
-            ({"id": int(df.loc[i, "id"])} for i in changes["deleted_rows"]),
+            "DELETE FROM players WHERE player_id = :player_id",
+            ({"player_id": int(df.loc[i, "player_id"])} for i in changes["deleted_rows"]),
         )
 
     conn.commit()
 
 
 # -----------------------------------------------------------------------------
-# Draw the actual page, starting with the inventory table.
+# Draw the actual page, starting with the players table.
 
 # Set the title that appears at the top of the page.
 """
-# :shopping_bags: Inventory tracker
+# :shopping_bags: NWO transfers
 
-**Welcome to Alice's Corner Store's intentory tracker!**
-This page reads and writes directly from/to our inventory database.
+**Welcome to NWO transfer**
+This page reads and writes directly from/to our clan database.
 """
 
 st.info(
@@ -202,13 +167,13 @@ edited_df = st.data_editor(
     num_rows="dynamic",  # Allow appending/deleting rows.
     column_config={
         # Show dollar sign before price columns.
-        "price": st.column_config.NumberColumn(format="$%.2f"),
-        "cost_price": st.column_config.NumberColumn(format="$%.2f"),
+        #"price": st.column_config.NumberColumn(format="$%.2f"),
+        #"cost_price": st.column_config.NumberColumn(format="$%.2f"),
     },
-    key="inventory_table",
+    key="player_table",
 )
 
-has_uncommitted_changes = any(len(v) for v in st.session_state.inventory_table.values())
+has_uncommitted_changes = any(len(v) for v in st.session_state.player_table.values())
 
 st.button(
     "Commit changes",
@@ -216,7 +181,7 @@ st.button(
     disabled=not has_uncommitted_changes,
     # Update data in database
     on_click=update_data,
-    args=(conn, df, st.session_state.inventory_table),
+    args=(conn, df, st.session_state.player_table),
 )
 
 
